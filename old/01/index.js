759eb72d7c095f4fda9ef619027173685af10acc
Mercia <merciayanmact@hotmail.com>
2019-04-11 23:26:06 -0300

    icons

---
+++ old/01/index.js
@@ -0,0 +1,27 @@
+function trigger(id) {
+    document.getElementById(id).classList.toggle('closed');
+};
+
+// let sections = {
+//   0: document.querySelector(`#section`),
+//   1: document.querySelector(`#section-2`),
+//   2: document.querySelector(`#section-3`),
+//   3: document.querySelector(`#section-4`),
+//   4: document.querySelector(`#section-5`),
+//   5: document.querySelector(`#section-6`)
+// };
+
+// setTimeout(
+//   window.addEventListener(`scroll`, function() {
+//     for (section in sections) {
+//       if (
+//         window.pageYOffset + (window.innerHeight * 3) / 3.5 > sections[section].offsetTop
+//       ) {
+//         sections[section].setAttribute(`style`, `opacity:1;transition-duration:1s`);
+//       } else {
+//         sections[section].setAttribute(`style`, `opacity:0;transition-duration:1s`);
+//       }
+//     }
+//   }),
+//   500
+// );
